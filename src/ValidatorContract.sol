d494d2478b840131e5dda5bf3d33a16a625102cb
Ivan <i.marchuk@entangle.fi>
2025-05-26 11:41:54 +0300

    feat: rewards draft

--- src/ValidatorContract.sol
+++ src/ValidatorContract.sol
@@ -9,23 +9,17 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {NonZeroLib} from "./lib/NonZeroLib.sol";
 
-
 /**
  * @title  Validator Contract
  * @notice Smart-contract for managing validator's licenses and rewards
  * @author Ivan M.
-*/
-contract ValidatorContract is
-    Initializable,
-    UUPSUpgradeable,
-    AccessControlUpgradeable
-{
+ */
+contract ValidatorContract is Initializable, UUPSUpgradeable, AccessControlUpgradeable {
     using SafeERC20 for IERC20;
-
 
     //
     //    ERRORS
-    // 
+    //
 
     error LicenseNotLocked(address user, uint256 tokenId);
     error DurationNotSet();
@@ -33,10 +27,9 @@ contract ValidatorContract is
     error InvalidPercent(uint256 percent);
     error UnlockCooldown();
 
-
     //
     //    EVENTS
-    // 
+    //
 
     event LicenseLocked(address from, uint256 licenseId);
     event LicenseUnlocked(address to, uint256 licenseId);
@@ -46,66 +39,81 @@ contract ValidatorContract is
 
     //
     //    CONSTANTS
-    // 
+    //
 
-    bytes32 public constant ADMIN  = keccak256("ADMIN");
+    bytes32 public constant ADMIN = keccak256("ADMIN");
     bytes32 public constant SETTER = keccak256("SETTER");
-
 
     /// @dev We want to have precision
     uint256 public constant MAX_PERCENT = 10_000;
+    uint256 public constant SHARE_DENOM = 1e18;
 
     /// @dev keccak256(abi.encode(uint256(keccak256("Validators.storage.ValidatorContract.Main")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant MAIN_STORAGE_LOCATION = 0x5381ff65829da98890ffb35a63efeb022dfb732b29445662c000933bcf078b00;
 
-
     //
     //    STORAGE
     //
-
     struct Lock {
-        bool    locked;
+        bool locked;
         uint256 lockedAt;
     }
 
     struct TokenSet {
         uint256[] ids;
-        mapping(uint256 tokenId => Lock) lock; 
+        mapping(uint256 tokenId => Lock) lock;
     }
 
     struct ValidatorSet {
         address[] validators;
-        TokenSet  licenseInfo;
+        mapping(address => TokenSet) licenseInfo;
     }
 
+    struct EpochData {
+        uint256 firstEpochReward;
+        uint256 firstEpochStart;
+        uint256 epochDuration;
+        uint256 lastEpochSeen;
+    }
+
+    struct EpochRewards {
+        uint256 rewardsUsed;
+        uint256 rewardsGiven;
+    }
+
+    struct Position {
+        uint256 accumulated;
+        uint256 rewardDebt;
+    }
+
+    struct RewardsData {
+        uint256 RPS;
+        uint256 totalLocked;
+        uint256 totalRewardsUsed;
+        uint256 totalRewardsGiven;
+        mapping(uint256 epoch => EpochRewards) epochRewards;
+        mapping(address validator => Position) position;
+    }
 
     struct Main {
         IERC20  rewardToken;
         IERC721 license;
-        uint256 firstEpochReward;
-        uint256 firstEpochStart;
-        uint256 epochDuration;
         uint256 decreasePercent;
-        uint256 totalRewardsGiven;
-        mapping(uint256 epoch => uint256) rewardsGiven;
-        mapping(address validator => ValidatorSet) validatorInfo;
+        EpochData    epochData;
+        RewardsData  rewardsData;
+        ValidatorSet validatorInfo;
     }
-
 
     //
     //    FUNCTIONS: Init
-    // 
+    //
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(
-        address _admin
-    )
-        initializer public
-    {
+    function initialize(address _admin) public initializer {
         __UUPSUpgradeable_init();
         __AccessControl_init();
 
@@ -113,41 +121,109 @@ contract ValidatorContract is
         _setRoleAdmin(ADMIN, SETTER);
 
         _grantRole(ADMIN, _admin);
-
     }
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyRole(ADMIN)
-        override
-    {}
-
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(ADMIN) {}
 
     //
     //    FUNCTIONS: NON-View
-    // 
+    //
 
-    function lockLicense(
-        uint256 tokenId
-    ) external {
+    function lockLicense(uint256 tokenId) external {
         Main storage $ = _getMainStorage();
         $.license.safeTransferFrom(msg.sender, address(this), tokenId);
 
-        TokenSet storage set = $.validatorInfo[msg.sender].licenseInfo;
+        TokenSet storage set = $.validatorInfo.licenseInfo[msg.sender];
+
+        // accumulate position rewards
+        Position storage position = $.rewardsData.position[msg.sender];
+        position.accumulated += validatorPendingRewardsRaw(msg.sender);
+
+        // update pool info 
+        $.rewardsData.totalLocked += 1;
 
         set.ids.push(tokenId);
         set.lock[tokenId].locked = true;
         set.lock[tokenId].lockedAt = block.timestamp;
 
+        // update RPS
+        uint256 newTotalLock = $.rewardsData.totalLocked;
+        uint256 pending = pendingPoolRewards();
+        uint256 rewardPerShareChange = (pending * SHARE_DENOM) / newTotalLock;
+
+        if (rewardPerShareChange > 0) {
+            $.rewardsData.RPS += rewardPerShareChange;
+
+            // "flush" rewards
+            EpochRewards storage rewardsData = $.rewardsData.epochRewards[nowEpoch()];
+            rewardsData.rewardsUsed += pending;
+        }
+
+        // update position
+        _updatePosition(msg.sender);
+
         emit LicenseLocked(msg.sender, tokenId);
     }
 
 
-    function unlockLicense(
-        uint256 tokenId
-    ) external {
+    /**
+     * @dev Update personal position based on pool RPS
+     */
+    function _updatePosition(
+        address validator
+    ) private {
         Main storage $ = _getMainStorage();
-        TokenSet storage set = $.validatorInfo[msg.sender].licenseInfo;
+        Position storage position = $.rewardsData.position[validator];
+        position.rewardDebt = _getPendingRaw(validator);
+    }
+
+
+    function validatorPendingRewards(
+        address validator
+    ) public view returns (uint256) {
+        // show data as RPS change already applied
+        Main storage $ = _getMainStorage();
+
+        uint256 totalLock = $.rewardsData.totalLocked;
+        uint256 pending = pendingPoolRewards();
+        uint256 rewardPerShareChange = (pending * SHARE_DENOM) / totalLock;
+
+        uint256 newRPS;
+        if (rewardPerShareChange > 0) {
+            newRPS = $.rewardsData.RPS + rewardPerShareChange;
+        }
+
+        uint256 locked = $.validatorInfo.licenseInfo[validator].ids.length;
+        return (locked * newRPS) / SHARE_DENOM;
+
+    }
+
+    function validatorPendingRewardsRaw(
+        address validator
+    ) public view returns (uint256) {
+        Main storage $ = _getMainStorage();
+        Position storage position = $.rewardsData.position[validator];
+        uint256 accumulatedReward = position.accumulated;
+        uint256 pendingRaw = _getPendingRaw(validator);
+
+        if (pendingRaw <= position.rewardDebt) {
+            return accumulatedReward;
+        }
+
+        return pendingRaw - position.rewardDebt + accumulatedReward;
+    }
+
+    function _getPendingRaw(
+        address validator
+    ) public view returns (uint256) {
+        Main storage $ = _getMainStorage();
+        uint256 locked = $.validatorInfo.licenseInfo[validator].ids.length;
+        return (locked * $.rewardsData.RPS) / SHARE_DENOM;
+    }
+
+    function unlockLicense(uint256 tokenId) external {
+        Main storage $ = _getMainStorage();
+        TokenSet storage set = $.validatorInfo.licenseInfo[msg.sender];
 
         if (!set.lock[tokenId].locked) {
             revert LicenseNotLocked(msg.sender, tokenId);
@@ -161,36 +237,28 @@ contract ValidatorContract is
             timePassed = 0;
         }
 
-        if (timePassed < $.epochDuration) {
+        if (timePassed < $.epochData.epochDuration) {
             revert UnlockCooldown();
         }
 
         // TODO: delete from token set
-
 
         $.license.transferFrom(address(this), msg.sender, tokenId);
 
         emit LicenseUnlocked(msg.sender, tokenId);
     }
 
-
     //
     //    FUNCTIONS: ADMIN & Roles
-    // 
+    //
 
-    function setRewardAndStart(
-        uint256 _firstEpochReward,
-        uint256 _start
-    ) 
-        external 
-        onlyRole(SETTER) 
-    {
+    function setRewardAndStart(uint256 _firstEpochReward, uint256 _start) external onlyRole(SETTER) {
         NonZeroLib._nonZeroV(_firstEpochReward);
         NonZeroLib._nonZeroV(_start);
 
         Main storage $ = _getMainStorage();
-        // check duration is set to prevent instant unlock
-        if ($.epochDuration == 0) {
+        // check duration is already set to prevent instant unlock
+        if ($.epochData.epochDuration == 0) {
             revert DurationNotSet();
         }
 
@@ -198,13 +266,11 @@ contract ValidatorContract is
             revert InvalidStart(_start);
         }
 
-        $.firstEpochStart = block.timestamp;
-        $.firstEpochReward = _firstEpochReward;
+        $.epochData.firstEpochStart = block.timestamp;
+        $.epochData.firstEpochReward = _firstEpochReward;
     }
 
-    function setDecreasePercent(
-        uint256 _decreasePercent
-    ) external onlyRole(SETTER) {
+    function setDecreasePercent(uint256 _decreasePercent) external onlyRole(SETTER) {
         NonZeroLib._nonZeroV(_decreasePercent);
 
         Main storage $ = _getMainStorage();
@@ -217,59 +283,50 @@ contract ValidatorContract is
         emit DecreasePercentSet(_decreasePercent);
     }
 
-
     /**
      * @notice Set epoch duration
      * @param _epochDuration - new epoch duration
      */
-    function setEpochDuration(
-        uint256 _epochDuration
-    ) external onlyRole(SETTER) {
+    function setEpochDuration(uint256 _epochDuration) external onlyRole(SETTER) {
         NonZeroLib._nonZeroV(_epochDuration);
         Main storage $ = _getMainStorage();
-        $.epochDuration = _epochDuration;
+        $.epochData.epochDuration = _epochDuration;
 
         emit EpochDurationSet(_epochDuration);
     }
 
-
     //
     //    UTILS
-    // 
+    //
 
-    function _getMainStorage() private pure returns(Main storage $) {
+    function _getMainStorage() private pure returns (Main storage $) {
         assembly {
             $.slot := MAIN_STORAGE_LOCATION
         }
     }
 
-
     //
     //    FUNCTIONS: Rewards
-    // 
-
+    //
 
     /**
      * @notice Calculate epoch rewards
-     * @dev    Use exponential decay for each epoch 
+     * @dev    Use exponential decay for each epoch
      * @param  epoch - epoch number
      * @return reward of X epoch
      */
-    function epochRewards(
-        uint256 epoch
-    ) public view returns (uint256) {
+    function epochRewards(uint256 epoch) public view returns (uint256) {
         Main storage $ = _getMainStorage();
-        uint256 initial = $.firstEpochReward;
+        uint256 initial = $.epochData.firstEpochReward;
 
         if (epoch == 0) return initial;
-        else
-            return (initial * $.decreasePercent ** epoch) / (MAX_PERCENT ** epoch);
+        else return (initial * $.decreasePercent ** epoch) / (MAX_PERCENT ** epoch);
     }
 
     function nowEpoch() public view returns (uint256) {
         Main storage $ = _getMainStorage();
-        uint256 start = $.firstEpochStart;
-        uint256 duration = $.epochDuration;
+        uint256 start = $.epochData.firstEpochStart;
+        uint256 duration = $.epochData.epochDuration;
         if (start > block.timestamp) {
             return 0;
         } else {
@@ -283,20 +340,41 @@ contract ValidatorContract is
 
     function currentRewardPerSecond() public view returns (uint256) {
         Main storage $ = _getMainStorage();
-        return currentFullRewards() / $.epochDuration;
+        return currentFullRewards() / $.epochData.epochDuration;
     }
 
-
-    function epochTimePassed() public view returns (uint256) {
-        Main storage $ = _getMainStorage();
+    function currentEpochTimePassed() public view returns (uint256) {
         uint256 ce = nowEpoch();
-        uint256 start = $.firstEpochStart + ce * $.epochDuration;
+        return epochTimePassed(ce);
+    }
 
-        return block.timestamp - start;
+    function epochTimePassed(uint256 epoch) public view returns(uint256) {
+        Main storage $ = _getMainStorage();
+
+        uint256 start = $.epochData.firstEpochStart + epoch * $.epochData.epochDuration;
+        if (block.timestamp > start + $.epochData.epochDuration) {
+            return $.epochData.epochDuration;
+        } else {
+            return block.timestamp - start;
+        }
+    }
+
+    function pendingEpochRewards(uint256 epoch) public view returns (uint256) {
+        Main storage $ = _getMainStorage();
+        EpochRewards storage rewardsData = $.rewardsData.epochRewards[epoch];
+        return currentRewardPerSecond() * epochTimePassed(epoch) - rewardsData.rewardsUsed;
     }
 
     function pendingPoolRewards() public view returns (uint256) {
         Main storage $ = _getMainStorage();
-        return currentRewardPerSecond() * epochTimePassed() - $.rewardsGiven[nowEpoch()];
+        uint256 lastSeen = $.epochData.lastEpochSeen;
+        uint256 current = nowEpoch();
+
+        uint256 totalPending;
+        for (uint256 i = lastSeen; i < current; i++) {
+            totalPending += pendingEpochRewards(i);
+        }
+
+        return totalPending;
     }
 }
--- src/lib/NonZeroLib.sol
+++ src/lib/NonZeroLib.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.28;
 
 library NonZeroLib {
-
     error ZeroAddress();
     error ZeroValue();
     error ZeroLength();
